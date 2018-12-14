b3ce6083ec3671b1484aca55d7693a8805e9acf6
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-12-14 12:20:59 +0100

    Files generated by PkgTemplates

---
+++ src/LatPhysLuttingerTiszaPlottingPyPlot.jl
@@ -0,0 +1,5 @@
+module LatPhysLuttingerTiszaPlottingPyPlot
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LatPhysLuttingerTiszaPlottingPyPlot
+using Test
+
+@testset "LatPhysLuttingerTiszaPlottingPyPlot.jl" begin
+    # Write your own tests here.
+end
